912bc716cbdf3b4ab96f55414138a9a92fcbd73c
Luke Hoang <lukehoang195@gmail.com>
2020-08-10 15:14:01 +0700

    nothing

--- src/containers/DashboardPage/index.js
+++ src/containers/DashboardPage/index.js
@@ -17,8 +17,8 @@ import ClipLoader from "react-spinners/ClipLoader";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import PaginationBar from "../../components/Pagination";
-import NameEditModal from '../../components/NameEditModal'
-import UpdateAvatarModal from '../../components/UpdateAvatarModal'
+import NameEditModal from "../../components/NameEditModal";
+import UpdateAvatarModal from "../../components/UpdateAvatarModal";
 
 // const PAGE_LIMIT = 5;
 const DashboardPage = () => {
@@ -27,9 +27,9 @@ const DashboardPage = () => {
   const blogs = useSelector((state) => state.blog.selfBlogs);
   const loading = useSelector((state) => state.blog.loading);
 
-  const [showModalName, setShowModalName] = useState(false)
-  const [showModalAvatar, setShowModalAvatar] = useState(false)
-  const user = useSelector(state => state.auth.user)
+  const [showModalName, setShowModalName] = useState(false);
+  const [showModalAvatar, setShowModalAvatar] = useState(false);
+  const user = useSelector((state) => state.auth.user);
 
   // const handleEditProfile = () => {
   //   alert("edit profile");
@@ -37,12 +37,12 @@ const DashboardPage = () => {
   // };
 
   const handleOnClickName = () => {
-    setShowModalName(true)
-  }
+    setShowModalName(true);
+  };
   const handleOnClickImage = () => {
-    console.log('handleOnClickImage')
-    setShowModalAvatar(true)
-  }
+    console.log("handleOnClickImage");
+    setShowModalAvatar(true);
+  };
 
   const handleClickOnBlog = (id) => {
     history.push(`/blogs/${id}`);
@@ -74,7 +74,10 @@ const DashboardPage = () => {
       />
       <PublicNavBar />
 
-      <Row className="body text-center justify-content-center dashboard" style={{ marginTop: "15px" }}>
+      <Row
+        className="body text-center justify-content-center dashboard"
+        style={{ marginTop: "15px" }}
+      >
         <div className="col-2 border-red profile">
           <h2>My Profile</h2>
           <Card style={{ width: "100%", marginTop: "15%" }}>
@@ -84,33 +87,31 @@ const DashboardPage = () => {
               roundedCircle
               alt=""
               style={{
-                height: "90px", width: "90px", marginTop: "10px"
+                height: "90px",
+                width: "90px",
+                marginTop: "10px",
               }}
             />
             <Card.Body className="text-center">
-              <Card.Title onClick={() => handleOnClickName()}>{user.name}</Card.Title>
+              <Card.Title onClick={() => handleOnClickName()}>
+                {user.name}
+              </Card.Title>
               <Card.Text>
                 <p style={{ fontSize: "15px" }}>currenlty in Saigon, Vietnam</p>
-                <p style={{ fontSize: "12px" }}>Traveler, Writer, Musician, Singer and Dreamer </p>
+                <p style={{ fontSize: "12px" }}>
+                  Traveler, Writer, Musician, Singer and Dreamer{" "}
+                </p>
               </Card.Text>
-
 
               {/* <Button variant="dark" onClick={() => handleEditProfile()}>
                 Edit
               </Button> */}
-
-
-
-
-
-
-
             </Card.Body>
           </Card>
         </div>
-        <div className="col-6 border-red" >
+        <div className="col-6 border-red">
           <h2>My Blogs</h2>
-          <Container style={{ marginTop: "15%" }} >
+          <Container style={{ marginTop: "15%" }}>
             <PaginationBar
               pageNum={pageNum}
               totalPageNum={totalPageNum}
@@ -119,22 +120,22 @@ const DashboardPage = () => {
             {loading ? (
               <ClipLoader color="#f86c6b" size={150} loading={loading} />
             ) : (
-                <>
-                  {blogs.length ? (
-                    <Card>
-                      {blogs.map((blog) => (
-                        <BlogCard
-                          blog={blog}
-                          key={blog._id}
-                          handleClick={handleClickOnBlog}
-                        />
-                      ))}
-                    </Card>
-                  ) : (
-                      <p>There are no blogs</p>
-                    )}
-                </>
-              )}
+              <>
+                {blogs.length ? (
+                  <Card>
+                    {blogs.map((blog) => (
+                      <BlogCard
+                        blog={blog}
+                        key={blog._id}
+                        handleClick={handleClickOnBlog}
+                      />
+                    ))}
+                  </Card>
+                ) : (
+                  <p>There are no blogs</p>
+                )}
+              </>
+            )}
           </Container>
         </div>
         <div className="col-2 border-red">
@@ -144,31 +145,43 @@ const DashboardPage = () => {
             <InputGroup.Prepend>
               <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl style={{ marginLeft: "-10px" }}
+            <FormControl
+              style={{ marginLeft: "-10px" }}
               placeholder="Search friends"
               aria-label="Username"
               aria-describedby="basic-addon1"
             />
-            <Button variant="dark" href="#" >Add</Button>
+            <Button variant="dark" href="#">
+              Add
+            </Button>
           </InputGroup>
           <Card style={{ width: "100%" }}>
             <Card.Body>
-              <Card.Title style={{ marginBottom: "-15px", marginTop: "-10px" }}>Pending Request</Card.Title>
+              <Card.Title style={{ marginBottom: "-15px", marginTop: "-10px" }}>
+                Pending Request
+              </Card.Title>
             </Card.Body>
             <ListGroup className="list-group-flush">
               <ListGroupItem>
                 <img src="" alt=" " />
-                waiting to be accepted  <Button variant="dark" style={{ fontSize: "13px" }}>Confirm</Button>
+                waiting to be accepted{" "}
+                <Button variant="dark" style={{ fontSize: "13px" }}>
+                  Confirm
+                </Button>
               </ListGroupItem>
               <ListGroupItem>
                 <img src="" alt=" " />
-                waiting to be accepted  <Button variant="dark" style={{ fontSize: "13px" }}>Confirm</Button>
+                waiting to be accepted{" "}
+                <Button variant="dark" style={{ fontSize: "13px" }}>
+                  Confirm
+                </Button>
               </ListGroupItem>
             </ListGroup>
 
             <Card.Body>
-
-              <Card.Title style={{ marginBottom: "-10px", paddingTop: "30px" }}>My Friends List</Card.Title>
+              <Card.Title style={{ marginBottom: "-10px", paddingTop: "30px" }}>
+                My Friends List
+              </Card.Title>
             </Card.Body>
             <ListGroup className="list-group-flush">
               <ListGroupItem>
